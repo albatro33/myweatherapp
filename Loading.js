6b2065c0adc42dab80ab538737b077507fd93a45
albatro33 <cokujin518@gmail.com>
2019-10-03 22:04:56 +0900

    Loading.js 생성, Expo-location 사용

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the weather to flight</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#ff4004"
+  },
+  text: {
+    color: "#f0f0f0",
+    fontSize: 30
+  }
+});
